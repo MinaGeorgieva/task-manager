51400c02952f0340b62e714eaff38fd50e4ed34f
М24287 <mina.georgieva@energo-pro.bg>
2019-07-15 13:32:01 +0300

    Add existing file

--- src/app/model/task.service.ts
+++ src/app/model/task.service.ts
@@ -2,8 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Task } from './task';
 import { Observable } from 'rxjs';
-//import 'rxjs/add/observable/of';
-//import 'rxjs/add/observable/from';
+
 import { HttpClient } from '@angular/common/http';
 import { of as observableOf} from 'rxjs';
 import { from as observableFrom} from 'rxjs';
@@ -24,44 +23,11 @@ const URL:string = 'http://localhost:3000';
 export class TaskService {
     newMode:boolean = false;
     constructor(private http:HttpClient) { }
-/*
-  getTasks():Task[]{
-    return this.tasks;
-  }
-*/
+
   getTaskList():Observable<Task[]>{
     //return observableOf(tasks);
     return this.http.get<Task[]>(`${URL}/task`);
   }
-/*
-  getTaskById(id:number):Observable<Task>{
-    let task:Task = tasks.filter((task=>task._id ===id)).pop();
-    return observableFrom([task]);
-  }
-
-  deleteTask(id:number):void{
-    console.log(`delete id:${id}`);
-    console.log(tasks);
-    tasks.splice(tasks.findIndex((task)=>task._id === id),1);
-    console.log(tasks);
-  }
-
-  saveTask(task:Task):void{
-    if(task._id){
-      tasks.forEach((tsk)=>{
-        if(tsk._id === task._id){
-         tsk = task;
-        
-        }
-      });
-    }
-    else{
-      task._id = tasks[tasks.length-1]._id + 1;
-      task.lastModified = Date.now();
-      tasks.push(task);
-      console.log(tasks);
-    }
-  */
 
  getTaskById(id:string):Observable<Task>{
   return this.http.get<Task>(`${URL}/task/${id}`);
